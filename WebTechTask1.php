4b88ae7bfc7e649fe682f4e70f82f9ba0b7937d3
TA91Anastasia <90056676+TA91Anastasia@users.noreply.github.com>
2023-01-10 21:58:24 +0200

    Add files via upload

---
+++ WebTechTask1.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=, initial-scale=1.0">
+    <title>PHP</title>
+</head>
+<body>
+    <?php
+    echo "Task 1<br>\n";
+    echo($_GET['key']);
+    
+    ?>
+</body>
+</html>
